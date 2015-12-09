7b1bc5d52b1f155169464a4c80f330c64c9fe2b8
Matt Stephens <mfstephens9@gmail.com>
2015-12-09 00:10:49 -0500

    problems 1 through 6

---
+++ advent-of-code.py
@@ -0,0 +1,305 @@
+# ````````````````````````````````````````````````````````````````````````````````````````````````````
+# ````````````````````````````````````````````````````````````````````````````````````````````````````
+# ````````````````````````````````````````````````````````````````````````````````````````````````````
+# ``````````I'M A GENTILE`````````````````````````````````````````````````````````````````````````````
+# ```````````````````````````````````````````````.,,,,,,,.````````````````````````````````````````````
+# `````````````````````````````````````````````.,,,,.,,,,,,,``````````````````````````````````````````
+# ````````````````````````````````````````````,,,,,.....,,,,,`````````````````````````````````````````
+# ```````````````````````````````````````````,,,,,........,,,,.```````````````````````````````````````
+# `````````````````````````````````````````.,,,,,.........,,,,:.``````````````````````````````````````
+# ````````````````````````````````````````.,,,,,,..........,,,::..````````````````````````````````````
+# ```````````````````````````````````````.,,,,,,....```.....,,,::..```````````````````````````````````
+# ```````````````````````````````````````,,,,,,,...`````....,,,::,.```````````````````````````````````
+# ``````````````````````````````````````.,,,,,,.............,,,:::,.``````````````````````````````````
+# ``````````````````````````````````````.,,,,,,.............,,,:::,.``````````````````````````````````
+# ``````````````````````````````````````,,,,,,.,............,,,:;:,,.`````````````````````````````````
+# `````````````````````````````````````..,,,,,.....``.`......,,:;:,,,.````````````````````````````````
+# `````````````````````````````````````..,,,,,........`......,,:;:,,,,.```````````````````````````````
+# ````````````````````````````````````...,,,:,..,,::,..,,:::,,,,::,,,,.```````````````````````````````
+# ````````````````````````````````````...,,,,,::''';;:.,;;''';;:::,,,..```````````````````````````````
+# ```````````````````````````````````...,:::':::+@'';,.,;'+@+;;;;;,,,,.```````````````````````````````
+# ```````````````````````````````````...,::::::'+'::;,.,;::+'':;;:,..,.```````````````````````````````
+# ```````````````````````````````````.....,,:,:,,,,,:,.,:.,,,,::::...,.```````````````````````````````
+# ```````````````````````````````````.....,,,,,,..,.:,.,:,..,,:,::.....```````````````````````````````
+# ````````````````````````````````````....,,,,,....,,,.,:,.,,,,,::,..`.```````````````````````````````
+# ```````````````````````````````````..,..:,,,,....,,,..,,,,.,,,::,,,.````````````````````````````````
+# ````````````````````````````````````,,,.,,,,,,,:..,,..,,.:,,:,:;,.,.````````````````````````````````
+# ````````````````````````````````````..:.,,,,,....,,,..,:...,,,::..,`````````````````````````````````
+# `````````````````````````````````````.:..:,,,...,,'+:,+'...,,,::.,.`````````````````````````````````
+# `````````````````````````````````````.,..:,,,.....::'+':..,,,,:,,,``````````````````````````````````
+# `````````````````````````````````````,,,.:,,,,.,..,,,,,,...,,,;,:,``````````````````````````````````
+# ````````````````` ```````````````````.,:::,,,,,,,,,.,.,,,,,,,:;;:,``````````````````````````````````
+# ``````````````````````````````````````,:;::,,,,.,,:,.,,,,,,,,:;';```````````````````````````````````
+# ```````````````` `````````````````````.,,::,,,,,:@'''++;:,,,,:;:,.``````````````````````````````````
+# ```````````````````````````````````````..,::,,,,,,,,.,,,,;,,:;:.,```````````````````````````````````
+# `````````````````````````````````````````.::,,,,,,,,,,::,,,,:;``.```````````````````````````````````
+# ``````````````````````````````````````````;;:,,,,,,,,,,,,::,;'``````````````````````````````````````
+#  `````````````````````````````````````````:+;:,,,,,,,,,,,:::'+``````````````````````````````````````
+# `````````````````````````````````````````,''+;:,,,,..,,,,::;++.`````````````````````````````````````
+# ```` ```````````````````````````````````,@';++';:,,,,,,,:;'++'@,````````````````````````````````````
+#  `  ````````````````````````````````````@@';'++'':::,,,:;@@++'@@````````````````````````````````````
+#      `````````````````````````````````:+@@';;'++'+'''''+@@++''@@@```````````````````````````````````
+#      ```````````````````````````````.+@@@@';;;''''+++'++@@+'''+@@@.`````````````````````````````````
+#    ` ` ``````````````````````` ```:@@@@@@@;:;:;;;;''''++@+';;;@@@@@;````````````````````````````````
+#     `       ````````````````````;@@@@@@@@@::::::;;';''+++';;;;@@@@@@@'.`````````````````````````````
+#              `````````````````'@@@@@@@@@@@;:::::;;;;;'''';:::'@@@@@@@@@@:```````````````````````````
+#             `````````````` .+@@@@@@@@@@@@@:::::,:;:;;;;;;::::+@@@@@@@@@@@@;`````````````````````````
+#             `````````````,+@@@@@@@@@@@@@@@'::::,:::;::;;::::+@@@@@@@@@@@@@@@:```````````````````````
+#                ````````:@@@@@@@@@@@@@@@@@@@;,::,,:;::::::::++@@@@@@@@@@@@@@@@+``````````````````````
+#                 ```` :@@@@@@@@@@@@@@@@@@@@@@;::,,::::::,:;'++@@@@@@@@@@@@@@@@@@:```````` ```````````
+#                    :@@@@@@@@@@@@@@@@@@@@@@+@@+:,,:,,::,;+@@@@@@@@@@@@@@@@@@@@@@@+```````````````````
+#                  `+@@@@@@@@@@@@@@@@@@@@@@@@+@@@@;,,,,:+@+@@+@@@@@@@@@@@@@@@@@@@@@@,`````````````````
+#                  ;@@@@@@@@@@@@@@@@@@@@@@@@@@+@@@@@@@@+@'++++@@@@@@@@@@@@@@@@@@@@@@@;````````````````
+#                 `+@@@@@@@@@@@@@@@@@@@@@@@@@+++@@@@@@+@+++@++@@@@@@@@@@@@@@@@@@@@@@@@@```````````````
+#                 :+@@@@@@@@@@@@@@@@@@@@@@@@@++++@@@@@++++++''@@@@@@@@@@@@@@@@@@@@@@@@@+```````  `````
+#                `+@@@@@@@@@@@@@@@@@@@@@@@@@@+++++@@@+++++''''@@@@@@@@@@@@@@@@@@@@@@@@@@.````   `   ``
+#  `  `          `+@@@@@@@@@@@@@@@@@@@@@@@@@@+++'+@@@+'''+'+'+@@@@@@@@@@@@@@@@@@@@@@@@@@'``` `    ` ``
+#                ,+@@@@@@@@@@@@@@@@@@@@@@@@@@+''''+@+''++''''@@@@@@@@@@@@@@@@@@@@@@@@@@@@```          
+#       `        '@@@@@@@@@@@@@@@@@@@@@@@@@@@@'+'''.+'''+++++@@@@@@@@@@@@@@@@@@@@@@@@@@@@```  ``      
+#                @@@@@@@@@@@@@@@@@@@@@@@@@@@@@'+''':+'''+++++@@@@@@@@@@@@@@@@@@@@@@@@@@@@``````       
+#        `      `+@@@@@@@@@@@@@@@@@@@@@@@@@@@@+++'';'''+''+'+@@@@@@@@@@@@@@@@@@@@@@@@@@@@. `          
+#               `+@@@@@@@@@@@@@@@@@@@@@@@@@@@@++'''+''''+'++@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:        `   
+#               .+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@+'''+'''+''''@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'  `      `  
+#               :+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@+'''+''''''++@@@@@@@@@@@@@@@@@@@@@@@@@@@@@+            
+#          `    ;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@+''';''++'''+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@            
+#            `  '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@''''+''''''+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@            
+#        `      +@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@+'''''''''''@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@            
+#        `      ++@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@''''''''''+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@            
+
+import hashlib
+from ast import literal_eval
+
+def isNegative(x):
+    return x < 0
+
+def dayOne(partTwo=False):
+    f = open('day-1-input', 'r')
+    input = f.read()
+    floor = 0
+    position = 0
+    for i in input:
+        if partTwo and isNegative(floor):
+            return position
+        if i == '(':
+            floor += 1
+        else:
+            floor -= 1
+        position += 1
+    f.close()
+    return floor
+
+def dayOnePartTwo():
+    return dayOne(partTwo=True)
+
+def calcSurfaceArea(sideA, sideB):
+    return sideA * sideB * 2
+
+def calcArea(x, y, z):
+    return x * y * z
+
+def dayTwo():
+    f = open('day-2-input', 'r')
+    surfaceArea = 0
+    for line in f:
+        dimensions = [int(x) for x in line.split('x')]
+        surfaceArea += calcSurfaceArea(dimensions[0], dimensions[1]) 
+        surfaceArea += calcSurfaceArea(dimensions[1], dimensions[2]) 
+        surfaceArea += calcSurfaceArea(dimensions[0], dimensions[2])
+        dimensions.remove(max(dimensions))
+        surfaceArea += dimensions[0] * dimensions[1]
+    f.close()
+    return surfaceArea
+
+def dayTwoPartTwo():
+    f = open('day-2-input', 'r')
+    ribbonLength = 0
+    for line in f:
+        dimensions = [int(x) for x in line.split('x')]
+        ribbonLength += calcArea(dimensions[0], dimensions[1], dimensions[2])
+        dimensions.remove(max(dimensions))
+        ribbonLength += (dimensions[0] * 2) + (dimensions[1] * 2)
+    f.close()
+    return ribbonLength
+
+def getNewPoint(direction, x, y):
+    return {
+        '^': (x, y + 1),
+        '>': (x + 1, y),
+        'v': (x, y - 1),
+        '<': (x - 1, y)
+    }.get(direction)
+
+def getNewPresentsValue(presents):
+    if presents != None:
+        return (presents + 1)
+    else:
+        return 1
+
+def dayThree():
+    f = open('day-3-input', 'r')
+    input = f.read()
+    currentPoint = (0,0)
+    visitedPoints = {currentPoint: 1}
+    for i in input:
+        currentPoint = getNewPoint(i, currentPoint[0], currentPoint[1])
+        presents = visitedPoints.get(currentPoint)
+        visitedPoints[currentPoint] = getNewPresentsValue(presents)
+
+    f.close()
+    return sum(x[1] >= 1 for x in visitedPoints.items())
+
+def dayThreePartTwo():
+    f = open('day-3-input', 'r')
+    input = f.read()
+    visitedPoints = {(0,0): 2}
+    currentPointA = (0,0)
+    currentPointB = (0,0)
+    move = 1
+    for i in input:
+        if move % 2 == 0:
+            currentPointA = getNewPoint(i, currentPointA[0], currentPointA[1])
+            presentsA = visitedPoints.get(currentPointA)
+            visitedPoints[currentPointA] = getNewPresentsValue(presentsA)
+        else:
+            currentPointB = getNewPoint(i, currentPointB[0], currentPointB[1])
+            presentsB = visitedPoints.get(currentPointB)
+            visitedPoints[currentPointB] = getNewPresentsValue(presentsB)
+        move += 1
+
+    f.close()
+    return sum(x[1] >= 1 for x in visitedPoints.items())
+
+def dayFour(partTwo=False):
+    prefixCheck = "00000"
+    prefixBuffer = 5
+    if partTwo:
+        prefixCheck = "000000"
+        prefixBuffer = 6
+    m = hashlib.md5()
+    input = "iwrupvqb"
+    suffix = 0
+    hashable = input + str(suffix)
+    while True:
+        m.update(hashable)
+        prefix = m.hexdigest()[:prefixBuffer]
+        if prefix == prefixCheck:
+            return hashable
+        suffix += 1
+        hashable = input + str(suffix)
+        m = hashlib.md5()
+
+def dayFourPartTwo():
+    return dayFour(True)
+
+def isGoodString(string):
+    goodVowels = "aeiou"
+    badSubs = ["ab", "cd", "pq", "xy"]
+    vowels = 0
+    if any(x in string for x in badSubs):
+        return False
+    for g in goodVowels:
+        vowels += string.count(g)
+    if vowels < 3:
+        return False
+    for l in range(len(string) - 1):
+        if string[l] == string[l + 1]:
+            return True
+    return False
+
+def isGoodStringPartTwo(string):
+    condition1 = False
+    condition2 = False
+    for l in range(len(string) - 1):
+        if l >= 1:
+            if string[l - 1] == string[l + 1]:
+                condition1 = True
+        pair = string[l] + string[l + 1]
+        for j in range(len(string) -1):
+            if j not in [l - 1, l, l + 1]:
+                compare = string[j] + string[j + 1]
+                if pair == compare:
+                    condition2 = True
+
+    return condition1 and condition2
+
+def dayFiveProcedure(procedure):
+    f = open('day-5-input', 'r')
+    niceStrings = 0
+    for line in f:
+        if procedure(line):
+            niceStrings += 1
+    f.close()
+    return niceStrings
+
+def dayFive():
+    return dayFiveProcedure(isGoodString)
+
+def dayFivePartTwo():
+    return dayFiveProcedure(isGoodStringPartTwo)
+
+def toggleValue(x):
+    return x ^ 1
+
+def turnOff(x):
+    return 0
+
+def turnOn(x):
+    return 1
+
+def increaseByOne(x):
+    return x + 1
+
+def decreaseByOne(x):
+    if x - 1 >= 0:
+        return x - 1
+    else:
+        return 0
+
+def increaseByTwo(x):
+    return x + 2
+
+def setLightsWithOperator(grid, operator, corner1, corner2):
+    for x in range(corner1[0], corner2[0] + 1):
+        for y in range(corner1[1], corner2[1] + 1):
+            grid[y][x] = operator(grid[y][x])
+
+def countInGrid(grid):
+    on = 0
+    for x in grid:
+        for y in x:
+            on += y
+    return on
+
+def daySix():
+    # changed for part two
+    f = open('day-6-input', 'r')
+    grid = [[0 for x in range(1000)] for x in range(1000)]
+    for line in f:
+        command = line.split()
+        corner2 = tuple( int(i) for i in command[-1].split(',') )
+        if command[0] == "turn":
+            corner1 = tuple( int(i) for i in command[2].split(',') )
+            if command[1] == "off":
+                setLightsWithOperator(grid, decreaseByOne, corner1, corner2)
+            if command[1] == "on":
+                setLightsWithOperator(grid, increaseByOne, corner1, corner2)
+        if command[0] == "toggle":
+            corner1 = tuple( int(i) for i in command[1].split(',') )
+            setLightsWithOperator(grid, increaseByTwo, corner1, corner2)
+
+    return countInGrid(grid)
+
+def printSolution(day, part, solution):
+    print 'Day {0} part {1}: {2}'.format(day, part, solution)
+
+printSolution(1,1,dayOne())
+printSolution(1,2,dayOnePartTwo())
+printSolution(2,1,dayTwo())
+printSolution(2,2,dayOnePartTwo())
+printSolution(3,1,dayThree())
+printSolution(3,2,dayThreePartTwo())
+# printSolution(4,1,dayFour())
+# printSolution(4,2,dayFourPartTwo())
+printSolution(5,1,dayFive())
+printSolution(5,2,dayFivePartTwo())
+printSolution(6,1,daySix())
